c8b23da70b8b5955a70d1e2361ee19cc2de8919a
cjwattttts <cameron.jwatts@outlook.com>
2025-04-06 23:52:39 -0400

    Debug

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: []
+  plugins: [react()],
+  // base: '/my-app/', // Remove or change this line
 })
